a61dab466c2a467c037e19f1015a2c4550e9ec02
Michael McCain <michaelmc1989@gmail.com>
2022-12-26 10:26:29 -0500

    1. Fixed bugs with aiChoice function. 2. Renamed functions and arrays to meet naming standards. 3. Made minor changes to logic in main.

--- TicTacToe/TicTacToe_Source.cpp
+++ TicTacToe/TicTacToe_Source.cpp
@@ -21,29 +21,30 @@ using namespace std;
 const int SZ = 3;
 
 // Prototypes
-bool userChoiceValid (int *choice);									// Validates user grid choice
-void boardLayout(char play[SZ][SZ], int choice, bool *valid);		// Used to place grid choices
-void displayBoard(char board[SZ][SZ]);								// Used to display board
-void aiChoice(char play[SZ][SZ]);									// AI randomly picks available grid space
-void winCondition(char board[SZ][SZ]);
+bool User_Choice_Valid (int *choice);								// Validates user grid choice
+void Board_Layout(char play[SZ][SZ], int choice, bool *valid);		// Used to place grid choices
+void Display_Board(char board[SZ][SZ]);								// Used to display board
+void Ai_Choice(char play[SZ][SZ]);									// AI randomly picks available grid space
+bool Win_Condition(char board[SZ][SZ]);								// Checks for 3 in a row
+
 // Main
 int main()
 {
 	// Initalizers
-	char layoutGrid[SZ][SZ] =							// Board Layout w/ Grids
+	char Layout_Grid[SZ][SZ] =								// Board Layout w/ Grids
 	{
 		{'1','2','3'},
 		{'4','5','6'},
 		{'7', '8', '9'}
 	};
-	char layoutPlay[SZ][SZ] =							// Empty board layout used for play
+	char Layout_Play[SZ][SZ] =								// Empty board layout used for play
 	{
 		{' ',' ',' '},
 		{' ',' ',' '},
 		{' ',' ',' '}
 	};
-	bool closeWindow = false;							// Close window
-	bool valid = false;									// Validate choice
+	bool closeWindow = false;								// Close window
+	bool valid = false;										// Validate choice
 
 	// Display Title
 	cout << "\n	  -----------";
@@ -52,34 +53,34 @@ int main()
 	cout << "\n  -----------------------------\n" << endl;
 
 	// Display Board w/ grids
-	displayBoard(layoutGrid);
-
+	Display_Board(Layout_Grid);
 	while (!closeWindow) {
 		cout << endl;
 
 		// Reset grid choice
-		int choice = 0;									// Grid choice
+		int choice = 0;										// Grid choice
 		cout << endl;
 
 		// Gather user choice
 		do {
-			valid = userChoiceValid(&choice);
-			// Display play grid
-			boardLayout(layoutPlay, choice, &valid);	// Place grid choice
+			valid = User_Choice_Valid(&choice);				// Validate user Input
+			Board_Layout(Layout_Play, choice, &valid);		// Place grid choice
 		} while (valid == false);
-
-		// Have AI pick random spot
-		aiChoice(layoutPlay);
-		displayBoard(layoutPlay);						// Display updated gameboard
+		closeWindow = Win_Condition(Layout_Play);			// Check for win conditions
+		if (closeWindow == false)
+		{
+			Ai_Choice(Layout_Play);							// Have AI pick random spot
+			closeWindow = Win_Condition(Layout_Play);		// Check for win conditions
+		}
+		Display_Board(Layout_Play);							// Display updated gameboard
 		//system("pause");
-
 	}
 	return 0;
 }
 
 // Functions
 
-void displayBoard(char board[SZ][SZ])
+void Display_Board(char board[SZ][SZ])
 {
 	// Initalizers
 	int rowCount = 0;								// Count rows
@@ -114,10 +115,10 @@ void displayBoard(char board[SZ][SZ])
 	}
 }
 
-void aiChoice(char play[SZ][SZ])					// Needs solution for when all grids are full
+void Ai_Choice(char play[SZ][SZ])					
 {
 	// Initalizers
-	vector<int> gridOp;								// Store available grids for AI
+	vector<int> Grid_Op;								// Store available grids for AI
 	int gridSpot = 0;								// Track grid elements
 	
 	// Iterate through board, identify available grids, push gridSpot to vector
@@ -126,48 +127,83 @@ void aiChoice(char play[SZ][SZ])					// Needs solution for when all grids are fu
 		for (int j = 0; j < 3; j++)
 		{
 			if (play[i][j] != 'X' && play[i][j] != 'O') {
-				gridOp.push_back(gridSpot);
+				Grid_Op.push_back(gridSpot);
 			}
 			gridSpot++;
 		}
 	}
 
-	for (int i = 0; i < gridOp.size(); i++)
+	// If no grids are available, return to main
+	if (Grid_Op.size() == 0)
 	{
-		cout << gridOp[i];
+		return;
 	}
-	cout << endl;
 
-	// Seed random number generator with time
-	srand(time(NULL));				
-	int aiPick = rand() % (gridOp.size() - 1);
-	aiPick = gridOp[aiPick];
+	// Seed random number generator with time and pick grid spot
+	srand(time(NULL));		
+	/*cout << endl << Grid_Op.size() << endl;*/
+	int aiPick = rand() % (Grid_Op.size());
+	aiPick = Grid_Op[aiPick];
 
 	if (aiPick >= 6 && aiPick <= 8) {
 		aiPick = aiPick - 6;
-		cout << ">5-AI pick is: " <<  aiPick << endl;
+		//cout << ">5-AI pick is: " <<  aiPick << endl;
 		play[2][aiPick] =  'O';
 	}
 	else if (aiPick >= 3 && aiPick <= 5)
 	{
 		aiPick = aiPick - 3;
-		cout << ">3-AI pick is: " << aiPick << endl;
+		//cout << ">3-AI pick is: " << aiPick << endl;
 		play[1][aiPick] = 'O';
 	}
 	else 
 	{
-		cout << "AI pick is: " << aiPick << endl;
+		//cout << "AI pick is: " << aiPick << endl;
 		play[0][aiPick] = 'O';
 	}
 
 }
 
-void winCondition(char board[SZ][SZ])
+bool Win_Condition(char play[SZ][SZ])					// AI win condition not working....
 {
+	bool winner = false;
+	// Check horizontal
+	for (int i = 0; i < 3; i++)
+	{
+		// Tally horizontal streaks
+		int playerStreak = 0;
+		int aiStreak = 0;
 
+		// Check for 3 X's or O's
+		for (int j = 0; j < 3; j++)
+		{
+			if (play[i][j] == 'X')
+			{
+				playerStreak++;
+				if (playerStreak == 3)
+				{
+					cout << "\nPlayer Wins!" << endl;
+					winner = true;
+					return winner;
+				}
+			}
+			else if (play[i][j] == 'O')
+			{
+				aiStreak = 0;
+				if (aiStreak == 3)
+				{
+					cout << "\nCPU Wins!" << endl;
+					winner = true;
+					return winner;
+				}
+			}
+			
+		}
+	}
+	return winner;
 }
 
-bool userChoiceValid( int *choice)
+bool User_Choice_Valid( int *choice)
 {
 	// Initializer
 	bool validation = false;
@@ -190,7 +226,7 @@ bool userChoiceValid( int *choice)
 	return validation;
 }
 
-void boardLayout(char play[SZ][SZ], int choice, bool *valid)
+void Board_Layout(char play[SZ][SZ], int choice, bool *valid)
 {
 	// Initalizer
 	string unavail = "Grid space is already occupied, please select a different grid space.\n";
